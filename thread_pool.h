7d6d69d682d64995a0ee579be7067598a0cff6d9
zhang wei <42429805+zzybooboo@users.noreply.github.com>
2019-02-19 00:01:24 +0800

    Update thread_pool.h
    注释

--- thread_pool.h
+++ thread_pool.h
@@ -16,14 +16,18 @@ author: zhangwei
 typedef void(*task_handler_t)(void *);
 typedef struct thread_pool_ * thread_pool_t;
 
-
+//创建线程池
 EXTERNC thread_pool_t  thread_pool_create(unsigned short min_count, unsigned short max_count);
 
+//回收资源
 EXTERNC void           thread_pool_shrink(thread_pool_t pool);
 
+//创建待执行任务
 EXTERNC bool           thread_pool_make_task(thread_pool_t pool, void * param, task_handler_t handler);
 
+//销毁线程池
 EXTERNC void           thread_pool_destory(thread_pool_t pool);
 
+//打印线程池状态报告
 EXTERNC char *         thread_pool_print_report(thread_pool_t pool);
 #endif
